974abaad25149c137f8b5f30ce831241237ce90e
faridamoussaeff <faridamus@protonmail.com>
2021-12-09 11:43:00 -0500

    Complete show component in index with stories and props

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,7 +8,10 @@ export default function Appointment(props) {
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? <Show /> : <Empty />}
+      {props.interview ? 
+      <Show 
+      student={props.interview.student}
+      interviewer={props.interview.interviewer} /> : <Empty />}
       {/* {props.time ? `Appointment at ${props.time}` : "No Appointments"} */}
     </article>
   );
