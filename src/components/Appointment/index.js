1f9fa9050e7390d4f85700063e568f7cf7a9f9d3
faridamoussaeff <faridamus@protonmail.com>
2021-12-09 11:39:10 -0500

    Empty component complete

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "components/Appointment/styles.scss"
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
   return (
     <article className="appointment">
-      {props.time ? `Appointment at ${props.time}` : "No Appointments"}
+      <Header time={props.time} />
+      {props.interview ? <Show /> : <Empty />}
+      {/* {props.time ? `Appointment at ${props.time}` : "No Appointments"} */}
     </article>
   );
 }
