089a0f4b85aea0daaeb994f9370ebfc332b5295c
faridamoussaeff <faridamus@protonmail.com>
2021-12-09 11:03:35 -0500

    Set default state with controlled component and controlled list

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("")
+    setInterviewer("")
+  }
+
+  const cancel = () => {
+    reset()
+    props.onCancel()
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -12,20 +25,20 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            /*
-              This must be a controlled component
-              your code goes here
-            */
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
           />
         </form>
         <InterviewerList 
-         /* your code goes here */
+        interviewers={props.interviewers}
+        value={interviewer}
+        onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
        <section className="appointment__actions">
-        <Button danger {/* your code goes here */}>Cancel</Button>
-        <Button confirm {/* your code goes here */}>Save</Button>
+       <Button danger onClick={cancel}>Cancel</Button>
+       <Button confirm onClick={props.onSave}>Save</Button>
        </section>
       </section>
     </main>
