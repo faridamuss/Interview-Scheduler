b6d043a35638e312b4bac760d35910437323de67
faridamoussaeff <faridamus@protonmail.com>
2021-12-09 11:08:23 -0500

    Populate Form with variables from props and state, add reset and cancel functions and fix onSubmit bug

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -8,7 +8,7 @@ export default function Form(props) {
 
   const reset = () => {
     setStudent("")
-    setInterviewer("")
+    setInterviewer(null)
   }
 
   const cancel = () => {
@@ -19,7 +19,7 @@ export default function Form(props) {
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -38,7 +38,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
        <section className="appointment__actions">
        <Button danger onClick={cancel}>Cancel</Button>
-       <Button confirm onClick={props.onSave}>Save</Button>
+       <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
        </section>
       </section>
     </main>
