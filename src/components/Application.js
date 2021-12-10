2e1f737ffaf9901b39b739ec45111a8f2033387d
faridamoussaeff <faridamus@protonmail.com>
2021-12-10 17:31:47 -0500

    Combine state, update props, create setDay and setDays function

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,8 +45,16 @@ const appointments = [
 
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState('Monday');
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // you may put the line below, but will have to remove/comment hardcoded appointments variable
+    // appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({...prev, days}))
+  
   useEffect(() => {
     axios.get("/api/days")
     .then((response) => {
@@ -65,8 +73,8 @@ export default function Application(props) {
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu">
 <DayList
-  days={days}
-  value={day}
+  days={state.days}
+  value={state.day}
   onChange={setDay}
 />
 </nav>
