69364f1f789a3204744e3b8d0fcaf03b4c5db1b3
faridamoussaeff <faridamus@protonmail.com>
2021-12-11 11:05:00 -0500

    Develop transition functionality

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,6 +2,11 @@ import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  
+  const transition = (newMode, ) => {
+    setMode(newMode);
 
-  return { mode };
-} 
+  }
+
+  return { mode, transition };
+}
