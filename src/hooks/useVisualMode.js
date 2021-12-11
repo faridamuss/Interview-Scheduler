044975120485107a1c7d8a68b4b757c6522460eb
faridamoussaeff <faridamus@protonmail.com>
2021-12-11 11:30:37 -0500

    Apply transition function, work with replace

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,15 +3,19 @@ import { useState } from "react";
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  
-  const transition = (newMode, ) => {
+
+  const transition = (newMode, replace = false) => {
+    if (!replace) {
+      setHistory((prev) => [...prev, newMode]);
+    }
     setMode(newMode);
   }
+
   const back = () => {
     if (history.length > 1) {
       history.pop();
       setMode(history[history.length - 1])
     }
-  }     
+  }
   return { mode, transition, back };
 }
