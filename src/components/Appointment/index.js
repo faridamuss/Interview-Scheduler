d4e129714a7e61d067fe954039e8c2079d15dce9
faridamoussaeff <faridamus@protonmail.com>
2021-12-11 11:37:09 -0500

    Conditionally render first components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,16 +3,36 @@ import "components/Appointment/styles.scss"
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+import { moduleExpression } from "@babel/types";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? 
+
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      />
+      )}
+
+      {/* {props.interview ? 
       <Show 
       student={props.interview.student}
-      interviewer={props.interview.interviewer} /> : <Empty />}
-      {/* {props.time ? `Appointment at ${props.time}` : "No Appointments"} */}
+      interviewer={props.interview.interviewer} /> : <Empty />} */}
+
     </article>
-  );
+      
+  ); 
 }
