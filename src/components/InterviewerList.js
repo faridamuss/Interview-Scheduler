e8ef685b2cc2d4346da3345ff4fdb1e1ffffe978
faridamoussaeff <faridamus@protonmail.com>
2021-12-14 15:33:24 -0500

    Import PropTypes to IntervierList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
+import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
   const interviewers = props.interviewers.map(interviewer => {
