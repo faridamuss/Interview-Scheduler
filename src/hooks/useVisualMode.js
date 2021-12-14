ca0b4faebb8f6804565cbffd25c0b763052e9d6e
faridamoussaeff <faridamus@protonmail.com>
2021-12-14 15:30:40 -0500

    Install prop-types dependency

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 
 export default function useVisualMode(initial) {
+  
   const [mode, setMode] = useState(initial);
+  
   const [history, setHistory] = useState([initial]);
 
   const transition = (newMode, replace = false) => {
@@ -13,9 +15,11 @@ export default function useVisualMode(initial) {
 
   const back = () => {
     if (history.length > 1) {
-      history.pop();
-      setMode(history[history.length - 1])
+      const newHistory = [...history];
+      newHistory.pop();
+      setHistory(newHistory);
+      setMode(newHistory[newHistory.length - 1]);
     }
-  }
+  };
   return { mode, transition, back };
 }
