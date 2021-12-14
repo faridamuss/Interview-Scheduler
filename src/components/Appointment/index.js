7ee45a3172f74bd42e8595503826426fb125f939
faridamoussaeff <faridamus@protonmail.com>
2021-12-13 22:13:03 -0500

    Refactor

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,7 +4,6 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
-import { moduleExpression } from "@babel/types";
 import Form from "./Form";
 
 const EMPTY = "EMPTY";
@@ -13,7 +12,7 @@ const CREATE = "CREATE";
 
 
 export default function Appointment(props) {
-  console.log('================', props)
+  console.log('===', props)
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
