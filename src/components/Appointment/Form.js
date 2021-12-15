111c1b0fb32a3e31512429856e24197c9b9b33f1
Farida MG <90786415+faridamoussaeff@users.noreply.github.com>
2021-12-15 11:47:05 -0500

    Form validates input onsubmit after tests

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,9 +5,11 @@ import Button from "components/Button";
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setStudent("")
+    setError("")
     setInterviewer(null)
   }
 
@@ -15,6 +17,16 @@ export default function Form(props) {
     reset()
     props.onCancel()
   }
+
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError("")
+    props.onSave(student, interviewer);
+  }
+
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -27,8 +39,10 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList 
          interviewers={props.interviewers}
          value={interviewer}
@@ -38,7 +52,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
        <section className="appointment__actions">
         <Button danger onClick={cancel}>Cancel</Button>
-        <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+        <Button confirm onClick={validate}>Save</Button>
        </section>
       </section>
     </main>
